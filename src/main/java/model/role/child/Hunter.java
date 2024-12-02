e2b24fe4f4c89e0cf2fa31c7a0243ed221b1ff1f
DESKTOP-2DNVOOR\240925PM <aklius14pg@gmail.cm>
2024-12-02 20:45:17 +0900

    ?

---
+++ src/main/java/model/role/child/Hunter.java
@@ -0,0 +1,11 @@
+package model.role.child;
+
+import model.role.person.Vills;
+
+public class Hunter extends Vills {
+	public Hunter() {
+		super();
+		this.setName("狩人");
+	}
+
+}
---
+++ src/main/java/model/role/child/Medium.java
@@ -0,0 +1,11 @@
+package model.role.child;
+
+import model.role.person.Vills;
+
+public class Medium extends Vills {
+	public Medium() {
+		super();
+		this.setName("霊能");
+	}
+
+}
